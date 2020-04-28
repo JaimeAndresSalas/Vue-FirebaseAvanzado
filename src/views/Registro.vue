2b8cf360be74e1bed5626fa5ca43eee1321db899
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-27 19:15:10 -0500

    Primeros cambios

---
+++ src/views/Registro.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Registro de Usuarios</h1>
+    <form >
+      <input type="email" v-model="email">
+      <input type="password" v-model="pass1">
+      <input type="password" v-model="pass2">
+      <button type="submit">Crear Usuario</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Registro',
+  data(){
+    return{
+      email:'',
+      pass1:'',
+      pass2:'',
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
